ae285d467294279d3aad1e3dba7088fd61dd8723
kaushal.basnet <kaushal.basnet@admin01012s-MacBook-Air.local>
2022-07-23 12:17:21 +0545

    translation completed

--- components/Translation/index.tsx
+++ components/Translation/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useTranslation } from "next-i18next";
 
 const Translation = () => {
+  const { t } = useTranslation();
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <p>welcome to Wesionary Team</p>
+      <h1>{t("Hello")}</h1>
+      <p>
+        {t("Welcome to")} Wesionary {t("Team")}
+      </p>
     </div>
   );
 };
